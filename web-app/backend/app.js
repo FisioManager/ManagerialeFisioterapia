9ebf9e0485cf5fb63ff0440cb457c4e9730daf67
figimodi <figimodi@gmail.com>
2023-02-10 00:48:34 +0100

    Inizzializato progetto React ed esempio back-end

---
+++ web-app/backend/app.js
@@ -0,0 +1,27 @@
+const http = require('http')    /* Retrive the library http by using require() */
+const fs = reqiure('fs')        /* File system library */
+const port = 7000
+
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' }) /* OK code, and specify content type */
+    fs.readFile('index.html', function(error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Error: File Not Found')  /* Instead of text we should render a 404 error page */
+        }
+        else {
+            res.write(data)
+        }
+        res.end()
+    })
+})
+
+/* Initialize the server instance listening on the given port */
+server.listen(port, function(error) {
+    if (error) {
+        console.log('Something went wrong', error)
+    }
+    else {
+        console.log('Server is listening on port ' + port)
+    }
+})
