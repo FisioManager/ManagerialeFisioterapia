832acaffdcd1d6b740f694e46e5cbdeb804eb977
figimodi <figimodi@gmail.com>
2023-02-13 23:30:13 +0100

    testato server con il bundle.jsx creato da react dopo la build

--- web-app/backend/server.jsx
+++ web-app/backend/server.jsx
@@ -1,27 +1,26 @@
-const http = require('http')    /* Retrive the library http by using require() */
+const express = require('express')  
 const fs = require('fs')        /* File system library */
+const app = express()
 const port = 3000
 
-const server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' }) /* OK code, and specify content type */
-    fs.readFile('index.html', function(error, data) {
-        if (error) {
-            res.writeHead(404)
-            res.write('Error: File Not Found')  /* Instead of text we should render a 404 error page */
-        }
-        else {
-            res.write(data)
-        }
-        res.end()
-    })
-})
-
-/* Initialize the server instance listening on the given port */
-server.listen(port, function(error) {
+app.listen(port, function(error) {
     if (error) {
         console.log('Something went wrong', error)
     }
     else {
         console.log('Server is listening on port ' + port)
     }
+})
+
+app.get('/index.html', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+app.get('/bundle.jsx', (req, res) => {
+    res.type('text/javascript');
+    res.sendFile(__dirname + '/public/bundle.jsx')
+  });
+
+app.get('/favicon.ico', (req, res) => {
+    res.sendFile(__dirname + '/public/favicon.ico')
 })
---
+++ web-app/frontend/README.md
@@ -0,0 +1,3 @@
+### *Comandi per buildare e runnare* ###
+- npx webpack (per buildare l'app)
+- npm start (per runnare l'app nel browser)
