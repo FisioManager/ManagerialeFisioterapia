d35782b505cd4329c063e2be09d6a64665b5c490
ldange00 <dng.lorenzo.00@gmail.com>
2023-02-14 20:25:41 +0100

    indentazione .jsx e rimozione const fs (non usata)

--- web-app/backend/server.jsx
+++ web-app/backend/server.jsx
@@ -1,26 +1,24 @@
-const express = require('express')  
-const fs = require('fs')        /* File system library */
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.listen(port, function(error) {
-    if (error) {
-        console.log('Something went wrong', error)
-    }
-    else {
-        console.log('Server is listening on port ' + port)
-    }
-})
-
-app.get('/index.html', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html')
+app.listen(port, function (error) {
+  if (error) {
+    console.log("Something went wrong", error);
+  } else {
+    console.log("Server is listening on port " + port);
+  }
 });
 
-app.get('/bundle.jsx', (req, res) => {
-    res.type('text/javascript');
-    res.sendFile(__dirname + '/public/bundle.jsx')
-  });
+app.get("/index.html", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
-app.get('/favicon.ico', (req, res) => {
-    res.sendFile(__dirname + '/public/favicon.ico')
-})
+app.get("/bundle.jsx", (req, res) => {
+  res.type("text/javascript");
+  res.sendFile(__dirname + "/public/bundle.jsx");
+});
+
+app.get("/favicon.ico", (req, res) => {
+  res.sendFile(__dirname + "/public/favicon.ico");
+});
