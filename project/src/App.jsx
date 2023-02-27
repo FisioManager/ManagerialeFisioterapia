cdfbda7a3b9411903e04a3b718771129b6706866
ldange00 <dng.lorenzo.00@gmail.com>
2023-02-27 16:28:37 +0100

    accorpati componenti da routare

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,31 +1,32 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  Header,
+  Footer,
+  Home,
+  ChiSiamo,
+  Contattaci,
+  Documentazione,
+  Prodotti,
+  Login,
+} from "./components";
+
 import "./App.scss";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Home from "./pages/Home/Home";
-import ChiSiamo from "./pages/ChiSiamo/ChiSiamo";
-import Contattaci from "./pages/Contattaci/Contattaci";
-import Documentazione from "./pages/Documentazione/Documentazione";
-import Prodotti from "./pages/Prodotti/Prodotti";
-import Login from "./pages/Login/Login";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/chi-siamo" element={<ChiSiamo />} />
-          <Route exact path="/contattaci" element={<Contattaci />} />
-          <Route exact path="/prodotti" element={<Prodotti />} />
-          <Route exact path="/log-in" element={<Login />} />
-          <Route exact path="/documentazione" element={<Documentazione />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/chi-siamo" element={<ChiSiamo />} />
+        <Route exact path="/contattaci" element={<Contattaci />} />
+        <Route exact path="/prodotti" element={<Prodotti />} />
+        <Route exact path="/log-in" element={<Login />} />
+        <Route exact path="/documentazione" element={<Documentazione />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/components.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import ChiSiamo from "./pages/ChiSiamo/ChiSiamo";
+import Contattaci from "./pages/Contattaci/Contattaci";
+import Documentazione from "./pages/Documentazione/Documentazione";
+import Prodotti from "./pages/Prodotti/Prodotti";
+import Login from "./pages/Login/Login";
+
+export {
+  Header,
+  Footer,
+  Home,
+  ChiSiamo,
+  Contattaci,
+  Documentazione,
+  Prodotti,
+  Login,
+};
