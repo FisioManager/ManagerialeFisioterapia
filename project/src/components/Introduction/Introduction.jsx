44d95b835e676a44d397e2698d4e297153c863c0
figimodi <figimodi@gmail.com>
2023-02-27 15:15:54 +0100

    creato componente Introduction

---
+++ project/src/components/Introduction/Introduction.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Introduction = () => {
+  return (
+    <div id="introduction">
+      <h1>Mega Titolo catchy che introduce il sito</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel
+        ligula venenatis, vehicula velit id, aliquet nibh.
+      </p>
+    </div>
+  );
+};
+
+export default Introduction;
--- project/src/pages/Home/Home.jsx
+++ project/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Section from "./../../components/Section/Section";
+import Introduction from "./../../components/Introduction/Introduction";
 
 const Home = () => {
   return (
     <>
       <div id="body">
+        <Introduction />
         <Section />
       </div>
     </>
