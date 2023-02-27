464830a2f678cb9bae64fb20dc624ea92040babe
figimodi <figimodi@gmail.com>
2023-02-27 19:50:51 +0100

    aggiunta seconda section

--- project/src/components/Section/Section.jsx
+++ project/src/components/Section/Section.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 
-const Section = ({src, titolo, testo}) => {
+const Section = ({ src, titolo, testo, mode }) => {
   return (
-    <div id="section">
-      <div id="illustration">
-        <img src={src} alt="illustration" />
-      </div>
-      <div id="description">
-        <h1>{titolo}</h1>
-        <p>
-          {testo}
-        </p>
-      </div>
+    <div id="section" className={mode ? "dark" : "light"}>
+      {mode ? (
+        <>
+          <div id="illustration">
+            <img src={src} alt="illustration"/>
+          </div>
+          <div id="description">
+            <h1>{titolo}</h1>
+            <p>{testo}</p>
+          </div>
+        </>
+      ) : (
+        <>
+          <div id="description">
+            <h1>{titolo}</h1>
+            <p>{testo}</p>
+          </div>
+          <div id="illustration">
+            <img src={src} alt="illustration" />
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- project/src/pages/Home/Home.jsx
+++ project/src/pages/Home/Home.jsx
@@ -13,6 +13,15 @@ const Home = () => {
           testo={
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id tincidunt sapien, a pretium justo. Praesent at hendrerit tortor, eget efficitur urna. Vestibulum imperdiet lectus nibh, eget interdum dui sollicitudin non. In scelerisque euismod nisl sit amet cursus. Ut ut massa ac est porta finibus."
           }
+          mode={true}
+        />
+        <Section
+          src={"/images/place-holder-500.png"}
+          titolo={"Titolo della seconda descrizione"}
+          testo={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id tincidunt sapien, a pretium justo. Praesent at hendrerit tortor, eget efficitur urna. Vestibulum imperdiet lectus nibh, eget interdum dui sollicitudin non. In scelerisque euismod nisl sit amet cursus. Ut ut massa ac est porta finibus."
+          }
+          mode={false}
         />
       </div>
     </>
