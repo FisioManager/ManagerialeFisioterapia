9127451e31bf12b4f82d84602d0c7f2c6317e1fe
ldange00 <dng.lorenzo.00@gmail.com>
2023-03-01 23:11:53 +0100

    rivisitato Header

--- project/src/components/Header/Header.jsx
+++ project/src/components/Header/Header.jsx
@@ -1,30 +1,25 @@
-import React from "react";
+import "./Header.scss";
+
+//import React from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <>
-      <div id="header">
-        <img src="/images/logo.svg" alt="logo" />
-        <div id="navbar">
-            <Link to="/">
-              Home
-            </Link>
-            <Link to="/chi-siamo">
-              Chi Siamo
-            </Link>
-            <Link to="/contattaci">
-              Contattaci
-            </Link>
-            <Link to="/Prodotti">
-              Prodotti
-            </Link>
-            <Link to="/log-in">
-              Log In
-            </Link>
-        </div>
-      </div>
-    </>
+    <header className="flex center">
+      <Link to="/">
+        <img src="/images/logo.svg" alt="logo aziendale" />
+      </Link>
+
+      <div className="empty"></div>
+
+      <nav className="flex gap-2">
+        <Link to="/">Home</Link>
+        <Link to="/chi-siamo">Chi Siamo</Link>
+        <Link to="/contattaci">Contattaci</Link>
+        <Link to="/Prodotti">Prodotti</Link>
+        <Link to="/log-in">Log In</Link>
+      </nav>
+    </header>
   );
 };
 
