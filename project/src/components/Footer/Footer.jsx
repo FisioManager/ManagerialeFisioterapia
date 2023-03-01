29a6cee51ef6173ac35dfd720bf837e560751ea5
ldange00 <dng.lorenzo.00@gmail.com>
2023-03-01 23:12:09 +0100

    rivisitato Footer

--- project/src/components/Footer/Footer.jsx
+++ project/src/components/Footer/Footer.jsx
@@ -1,32 +1,31 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div id="footer">
-      <ul>
-        <li>GDPR</li>
-        <li>Privacy Policy</li>
-        <li>Cookie Policy</li>
-        <li>Terms Of Service</li>
-      </ul>
-      <ul>
-        <li>Aiuto</li>
-        <li>Email</li>
-        <li>
-          <Link to="/documentazione" className="link">
-            Documentazione
-          </Link>
-        </li>
-        <li>Mappa Del Sito</li>
-      </ul>
-      <ul>
-        <li>
-          <img src="/images/logo.svg" alt="logo" />
-        </li>
-        <li>&#169; 2023 | Copyright</li>
-      </ul>
-    </div>
+    <footer className="flex center gap-5">
+      <nav className="flex gap-5">
+        <div className="flex col gap">
+          <Link to="">GDPR</Link>
+          <Link to="">Privacy Policy</Link>
+          <Link to="">Cookie Policy</Link>
+          <Link to="">Termini di servizio</Link>
+        </div>
+        <div className="flex col gap">
+          <Link to="">Aiuto</Link>
+          <Link to="">Email</Link>
+          <Link to="">Documentazione</Link>
+          <Link to="/documentazione">Mappa Del Sito</Link>
+        </div>
+      </nav>
+
+      <div>
+        <Link to="/">
+          <img src="/images/logo.svg" alt="logo aziendale" />
+        </Link>
+        <p>&#169; 2023 | Copyright</p>
+      </div>
+    </footer>
   );
 };
 
